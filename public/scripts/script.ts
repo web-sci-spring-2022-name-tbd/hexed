c459b6261d62246b3ed28ddb70280a975520d639
RussellABH <rabh.2202@gmail.com>
2022-02-15 11:04:59 -0500

    added jquery typing and changed document.ready

--- public/scripts/script.ts
+++ public/scripts/script.ts
@@ -1,5 +1,6 @@
-$(document).ready(function() {
+$(() => {
     $('#start').on('click', function(e) {
+        
         var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     });
 });
