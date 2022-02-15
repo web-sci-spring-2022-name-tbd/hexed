1e7984a7233608b2affef3b3728f26433b72cf78
RussellABH <rabh.2202@gmail.com>
2022-02-15 11:27:29 -0500

    getting name and time

--- public/scripts/script.ts
+++ public/scripts/script.ts
@@ -1,6 +1,14 @@
 $(() => {
     $('#start').on('click', function(e) {
-        
+        // Grab name and time
+        var userName  = String($("#nameInput").val());
+        var timeLimit = Number($("#timeInput").val());
+        if (userName == '') {
+            window.alert("Please enter a name");
+        } else if (timeLimit < 1 || timeLimit > 100) {
+            window.alert("Please enter a correct time");
+        }
+
         var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
     });
 });
