f4df5bf246afb8d9dbfb119d2bd64e787a101e8b
James Liu <liuxjames88@gmail.com>
2022-02-15 11:45:40 -0500

    adding inputs that show slider values

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,35 @@
 "use strict";
+// import $ from "jquery";
+function updateRed(val) {
+    $("#red_value").val(val);
+}
+function updateGreen(val) {
+    $("#green_value").val(val);
+}
+function updateBlue(val) {
+    $("#blue_value").val(val);
+}
+function startCountdown(seconds) {
+    let counter = seconds;
+    const interval = setInterval(() => {
+        $('#timer').val(counter);
+        counter--;
+        if (counter < 0) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 $(() => {
     $('#start').on('click', function (e) {
+        let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        $('#color-box').css('backgrouind-color', randomColor);
         // Grab name and time
         var userName = String($("#nameInput").val());
         var timeLimit = Number($("#timeInput").val());
@@ -10,6 +39,6 @@ $(() => {
         else if (timeLimit < 1 || timeLimit > 100) {
             window.alert("Please enter a correct time");
         }
-        var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        startCountdown(timeLimit);
     });
 });
